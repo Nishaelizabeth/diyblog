d2b26df6589d4d1da7e0ee91296838f2b2e0a1bf
Nisha Elizabeth Jacob <96191246+Nishaelizabeth@users.noreply.github.com>
2025-03-25 13:11:06 +0530

    Update settings.py

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -77,20 +77,12 @@ WSGI_APPLICATION = 'diyblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
